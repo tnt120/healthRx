2037af557eb1e02274f909e7966ed46f5403a4ea
tnt120 <apaj04@gmail.com>
2024-11-08 21:10:17 +0100

    [refactor] backend: statistics and user controllers & endpoints swagger missing description

--- backend/src/main/java/com/healthrx/backend/controller/StatisticsController.java
+++ backend/src/main/java/com/healthrx/backend/controller/StatisticsController.java
@@ -4,6 +4,7 @@ import com.healthrx.backend.api.external.raport.GenerateReportRequest;
 import com.healthrx.backend.api.external.statistics.*;
 import com.healthrx.backend.service.ReportService;
 import com.healthrx.backend.service.StatisticsService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -15,32 +16,37 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/statistics")
 @RequiredArgsConstructor
-@Tag(name = "Statistics controller", description = "Controller for managing statistics")
+@Tag(name = "Statistics controller", description = "Kontroler do zarządzania statystykami i generowania raportów")
 public class StatisticsController {
     private final StatisticsService statisticsService;
     private final ReportService reportService;
 
     @PostMapping("/chart")
+    @Operation(summary = "Pobranie danych do wykresu dla parametrów, leków lub aktywności", description = "Pobranie danych do wykresu dla parametrów, leków lub aktywności")
     public ResponseEntity<ChartResponse> getChartData (@RequestBody ChartRequest request) {
         return ResponseEntity.ok(statisticsService.getChartData(request, request.getType()));
     }
 
     @PostMapping("/parameters")
+    @Operation(summary = "Pobranie statystyk dla parametrów", description = "Pobranie statystyk dla parametrów")
     public ResponseEntity<List<ParameterStatisticsResponse>> getParametersStatistics (@RequestBody StatisticsRequest request) {
         return ResponseEntity.ok(statisticsService.getParametersStatistics(request));
     }
 
     @PostMapping("/drugs")
+    @Operation(summary = "Pobranie statystyk dla leków", description = "Pobranie statystyk dla leków")
     public ResponseEntity<List<DrugStatisticsResponse>> getDrugsStatistics (@RequestBody StatisticsRequest request) {
         return ResponseEntity.ok(statisticsService.getDrugsStatistics(request));
     }
 
     @PostMapping("/activities")
+    @Operation(summary = "Pobranie statystyk dla aktywności", description = "Pobranie statystyk dla aktywności")
     public ResponseEntity<List<ActivityStatisticsResponse>> getActivitiesStatistics (@RequestBody StatisticsRequest request) {
         return ResponseEntity.ok(statisticsService.getActivitiesStatistics(request));
     }
 
     @PostMapping("/generate-report")
+    @Operation(summary = "Generowanie raportu zdrowotnego", description = "Generowanie raportu zdrowotnego")
     public ResponseEntity<Void> generateReport(@RequestBody GenerateReportRequest req, HttpServletResponse response) {
         reportService.generateReport(req, response);
         return ResponseEntity.ok().build();
--- backend/src/main/java/com/healthrx/backend/controller/UserController.java
+++ backend/src/main/java/com/healthrx/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import com.healthrx.backend.api.external.UserVerificationRequest;
 import com.healthrx.backend.api.external.VerificationDataResponse;
 import com.healthrx.backend.handler.BusinessErrorCodes;
 import com.healthrx.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,17 +15,24 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@Tag(name = "User controller", description = "Controller for managing user data")
+@Tag(name = "User controller", description = "Kontroler do zarządzania danymi użytkownika w kontekście weryfikacji i inicjalizacji")
 public class UserController {
-
     private final UserService userService;
 
+    @GetMapping("/initAndConfig")
+    @Operation(summary = "Pobranie danych inicjalizacyjnych i konfiguracyjnych", description = "Pobranie danych inicjalizacyjnych i konfiguracyjnych")
+    public ResponseEntity<InitAndConfigResponse> getInitAndConfigData() {
+        return ResponseEntity.ok(userService.getInitAndConfigData());
+    }
+
     @PostMapping("/getVerificationData")
+    @Operation(summary = "Pobranie danych do zweryfikowania konta użytkownika", description = "Pobranie danych do zweryfikowania konta użytkownika")
     public ResponseEntity<VerificationDataResponse> getVerificationData(@RequestBody Token request) {
         return ResponseEntity.ok(userService.getVerificationData(request));
     }
 
     @PostMapping("/verification")
+    @Operation(summary = "Weryfikacja konta użytkownika", description = "Weryfikacja konta użytkownika")
     public ResponseEntity<Void> verifyUser(@RequestBody UserVerificationRequest request) {
         if (userService.verifyUser(request) == null) {
             throw BusinessErrorCodes.INVALID_VERIFICATION.getError();
@@ -32,10 +40,4 @@ public class UserController {
 
         return ResponseEntity.ok().build();
     }
-
-    @GetMapping("/initAndConfig")
-    public ResponseEntity<InitAndConfigResponse> getInitAndConfigData() {
-        return ResponseEntity.ok(userService.getInitAndConfigData());
-    }
-
 }
